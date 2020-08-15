8e692ac35580798d2c5c949c85ff78b385752e50
pietrushka <piotrekwisnia18@gmail.com>
2020-08-15 20:47:49 +0200

    add user helper functions and user join functionality

--- client/src/pages/canvas-page/canvas-page.component.jsx
+++ client/src/pages/canvas-page/canvas-page.component.jsx
@@ -1,11 +1,35 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import queryString from 'query-string' //help with retrieving data from url
 import io from 'socket.io-client'
 
 import Canvas from '../../components/canvas/canvas.component'
 
-const CanvasPage = () => {
-  const socket = io('http://localhost:4000')
+let socket
+
+const CanvasPage = ({location}) => {
+  const [name, setName] = useState('')
+  const [room, setRoom] = useState('')
+  const ENDPOINT = 'http://localhost:4000'
+
+  useEffect(() => {
+    const {name, room}  = queryString.parse(location.search)//parameters from url 
+
+    socket = io(ENDPOINT)
+
+    setName(name)
+    setRoom(room)
+
+    socket.emit('join', {name, room}, () => {
+      
+    })
+
+    //componentDidUnmount
+    return () => {
+      socket.emit('disconnect')
+      socket.off()
+    }
+  }, [ENDPOINT, location.search])
+
   return (
     <Canvas />
   )
--- server/server.js
+++ server/server.js
@@ -3,8 +3,14 @@ const socketio = require('socket.io')
 const http = require('http')
 const router = require('./router')
 
-const PORT = process.env.PORT || 4000
+const {
+  addUser,
+  removeUser,
+  getUser,
+  getUsersInRoom
+} = require('./users.js')
 
+const PORT = process.env.PORT || 4000
 
 const app = express()
 const server = http.createServer(app)
@@ -15,6 +21,19 @@ app.use(router)
 //Run when client connects
 io.on('connection', socket => { 
   console.log('user in')
+
+  socket.on('join', ({name, room}, callback) => {
+    const {error, user} = addUser({id: socket.id, name, room})
+
+    if(error) return callback(error)
+
+    socket.emit('message', {user: 'admin', text: `welcome to the room ${user.name}`})
+    socket.broadcast.to(user.room).emit('message', {user: 'admin', text: `${user.name} has joined`})
+
+    socket.join(user.room)
+
+    callback()
+  })
   
   // Runswhen clinet disconnect
   socket.on('disconnect', () => {
@@ -22,5 +41,5 @@ io.on('connection', socket => {
   })
 })
 
-server.listen(4000, () => console.log(`server runnin on port 4000`))
+server.listen(PORT, () => console.log(`server runnin on port 4000`))
 
