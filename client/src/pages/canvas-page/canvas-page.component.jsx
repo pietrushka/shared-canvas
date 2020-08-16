1ee569a9da15a9fb01aaa97a29e9cb1af2e10531
pietrushka <piotrekwisnia18@gmail.com>
2020-08-16 16:17:13 +0200

    added canvas

--- client/src/pages/canvas-page/canvas-page.component.jsx
+++ client/src/pages/canvas-page/canvas-page.component.jsx
@@ -1,38 +1,151 @@
-import React, {useState, useEffect} from 'react'
-import queryString from 'query-string' //help with retrieving data from url
-import io from 'socket.io-client'
+import React, { useRef, useEffect } from 'react';
+import io from 'socket.io-client';
 
-import Canvas from '../../components/canvas/canvas.component'
+import './canvas-page.css';
 
-let socket
 
-const CanvasPage = ({location}) => {
-  const [name, setName] = useState('')
-  const [room, setRoom] = useState('')
-  const ENDPOINT = 'http://localhost:4000'
+const Board = () => {
+  const canvasRef = useRef(null);
+  const colorsRef = useRef(null);
+  const socketRef = useRef();
 
   useEffect(() => {
-    const {name, room}  = queryString.parse(location.search)//parameters from url 
 
-    socket = io(ENDPOINT)
+    // --------------- getContext() method returns a drawing context on the canvas-----
 
-    setName(name)
-    setRoom(room)
+    const canvas = canvasRef.current;
+    const test = colorsRef.current;
+    const context = canvas.getContext('2d');
 
-    socket.emit('join', {name, room}, () => {
-      
-    })
+    // ----------------------- Colors --------------------------------------------------
 
-    //componentDidUnmount
-    return () => {
-      socket.emit('disconnect')
-      socket.off()
+    const colors = document.getElementsByClassName('color');
+    console.log(colors, 'the colors');
+    console.log(test);
+    // set the current color
+    const current = {
+      color: 'black',
+    };
+
+    // helper that will update the current color
+    const onColorUpdate = (e) => {
+      current.color = e.target.className.split(' ')[1];
+    };
+
+    // loop through the color elements and add the click event listeners
+    for (let i = 0; i < colors.length; i++) {
+      colors[i].addEventListener('click', onColorUpdate, false);
     }
-  }, [ENDPOINT, location.search])
+    let drawing = false;
+
+    // ------------------------------- create the drawline ----------------------------
+
+    const drawLine = (x0, y0, x1, y1, color, emit) => {
+      context.beginPath();
+      context.moveTo(x0, y0);
+      context.lineTo(x1, y1);
+      context.strokeStyle = color;
+      context.lineWidth = 2;
+      context.stroke();
+      context.closePath();
+
+      if (!emit) { return; }
+      const w = canvas.width;
+      const h = canvas.height;
+
+      socketRef.current.emit('drawing', {
+        x0: x0 / w,
+        y0: y0 / h,
+        x1: x1 / w,
+        y1: y1 / h,
+        color,
+      });
+    };
+
+    // ---------------- mouse movement --------------------------------------
+
+    const onMouseDown = (e) => {
+      drawing = true;
+      current.x = e.clientX || e.touches[0].clientX;
+      current.y = e.clientY || e.touches[0].clientY;
+    };
+
+    const onMouseMove = (e) => {
+      if (!drawing) { return; }
+      drawLine(current.x, current.y, e.clientX || e.touches[0].clientX, e.clientY || e.touches[0].clientY, current.color, true);
+      current.x = e.clientX || e.touches[0].clientX;
+      current.y = e.clientY || e.touches[0].clientY;
+    };
+
+    const onMouseUp = (e) => {
+      if (!drawing) { return; }
+      drawing = false;
+      drawLine(current.x, current.y, e.clientX || e.touches[0].clientX, e.clientY || e.touches[0].clientY, current.color, true);
+    };
+
+    // ----------- limit the number of events per second -----------------------
+
+    const throttle = (callback, delay) => {
+      let previousCall = new Date().getTime();
+      return function() {
+        const time = new Date().getTime();
+
+        if ((time - previousCall) >= delay) {
+          previousCall = time;
+          callback.apply(null, arguments);
+        }
+      };
+    };
+
+    // -----------------add event listeners to our canvas ----------------------
+
+    canvas.addEventListener('mousedown', onMouseDown, false);
+    canvas.addEventListener('mouseup', onMouseUp, false);
+    canvas.addEventListener('mouseout', onMouseUp, false);
+    canvas.addEventListener('mousemove', throttle(onMouseMove, 10), false);
+
+    // Touch support for mobile devices
+    canvas.addEventListener('touchstart', onMouseDown, false);
+    canvas.addEventListener('touchend', onMouseUp, false);
+    canvas.addEventListener('touchcancel', onMouseUp, false);
+    canvas.addEventListener('touchmove', throttle(onMouseMove, 10), false);
+
+    // -------------- make the canvas fill its parent component -----------------
+
+    const onResize = () => {
+      canvas.width = window.innerWidth;
+      canvas.height = window.innerHeight;
+    };
+
+    window.addEventListener('resize', onResize, false);
+    onResize();
+
+    // ----------------------- socket.io connection ----------------------------
+    const onDrawingEvent = (data) => {
+      const w = canvas.width;
+      const h = canvas.height;
+      drawLine(data.x0 * w, data.y0 * h, data.x1 * w, data.y1 * h, data.color);
+    }
+
+    socketRef.current = io.connect('http://localhost:4000');
+    socketRef.current.on('drawing', onDrawingEvent);
+  }, []);
+
+  // ------------- The Canvas and color elements --------------------------
 
   return (
-    <Canvas />
-  )
-}
+    <div>
+      <canvas ref={canvasRef} className="whiteboard" />
 
-export default CanvasPage
+      <div ref={colorsRef} className="colors">
+        <div className="color black" />
+        <div className="color red" />
+        <div className="color green" />
+        <div className="color blue" />
+        <div className="color yellow" />
+      </div>
+    </div>
+  );
+};
+
+export default Board;
--- server/server.js
+++ server/server.js
@@ -19,27 +19,11 @@ const io = socketio(server)
 app.use(router)
 
 //Run when client connects
-io.on('connection', socket => { 
-  console.log('user in')
+io.on('connection', onConnection);
 
-  socket.on('join', ({name, room}, callback) => {
-    const {error, user} = addUser({id: socket.id, name, room})
-
-    if(error) return callback(error)
-
-    socket.emit('message', {user: 'admin', text: `welcome to the room ${user.name}`})
-    socket.broadcast.to(user.room).emit('message', {user: 'admin', text: `${user.name} has joined`})
-
-    socket.join(user.room)
-
-    callback()
-  })
-  
-  // Runswhen clinet disconnect
-  socket.on('disconnect', () => {
-    console.log('user out')
-  })
-})
+function onConnection(socket){
+  socket.on('drawing', (data) => socket.broadcast.emit('drawing', data));
+}
 
 server.listen(PORT, () => console.log(`server runnin on port 4000`))
 
