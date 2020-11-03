94fa7989d6378968d37f0504e3ec114882c1b868
pietrushka <piotrekwisnia18@gmail.com>
2020-11-03 20:08:01 +0100

    Move whiteboard

--- src/Room/Room.jsx
+++ src/Room/Room.jsx
@@ -5,16 +5,14 @@ import io from 'socket.io-client'
 import { UserContext } from '../App'
 import RightPanel from './RightPanel'
 import './Room.scss'
+import Whiteboard from './Whiteboard'
 
 const RoomPage = ({ match }) => {
   const { user } = useContext(UserContext)
   const [messages, setMessages] = useState([]);
-  const { roomId } = match.params
   const socketRef = useRef()
-  const canvasRef = useRef(null)
-  const colorsRef = useRef(null)
-  const eraserRef = useRef(null)
-  
+  const whiteboardRef = useRef()
+  const { roomId } = match.params
   const SERVER_ENDPOINT = process.env.REACT_APP_SERVER_ENDPOINT
   
   useEffect(() => {
@@ -28,141 +26,23 @@ const RoomPage = ({ match }) => {
       }
     })
 
-    // --------------- getContext() method returns a drawing context on the canvas-----
-    const canvas = canvasRef.current
-    const context = canvas.getContext('2d')
-
-    // ----------------------- Colors --------------------------------------------------
-    const eraser = document.getElementById('eraser')
-    const colors = document.getElementsByClassName('color')
-
-    // set the current color
-    const current = {
-      color: 'black'
-    }
-
-    const getEraser = () => {
-      current.color = 'eraser'
-    }
-
-    eraser.addEventListener('click', getEraser, false)
-
-    // helper that will update the current color
-    const onColorUpdate = (e) => {
-      current.color = e.target.className.split(' ')[1]
-    }
-
-    // loop through the color elements and add the click event listeners
-    for (let i = 0; i < colors.length; i++) {
-      colors[i].addEventListener('click', onColorUpdate, false)
-    }
-    let drawing = false
-
-    // ------------------------------- create the drawline ----------------------------
-
-    const drawLine = (x0, y0, x1, y1, color, emit) => {
-      if (color === 'eraser') {
-        context.clearRect(x0, y0, 10, 10)
-      } else {
-        context.beginPath()
-        context.moveTo(x0, y0)
-        context.lineTo(x1, y1)
-        context.strokeStyle = color
-        context.lineWidth = 2
-        context.stroke()
-        context.closePath()
-      }
-
-      if (!emit) { return }
-
-      const w = canvas.width
-      const h = canvas.height
-
-      socketRef.current.emit('drawing', {
-        x0: x0 / w,
-        y0: y0 / h,
-        x1: x1 / w,
-        y1: y1 / h,
-        color
-      })
-    }
-
-    // ---------------- mouse movement --------------------------------------
-
-    const onMouseDown = (e) => {
-      drawing = true
-      current.x = e.clientX || e.touches[0].clientX
-      current.y = e.clientY || e.touches[0].clientY
-    }
-
-    const onMouseMove = (e) => {
-      if (!drawing) { return }
-      drawLine(current.x, current.y, e.clientX || e.touches[0].clientX, e.clientY || e.touches[0].clientY, current.color, true)
-      current.x = e.clientX || e.touches[0].clientX
-      current.y = e.clientY || e.touches[0].clientY
-    }
-
-    const onMouseUp = (e) => {
-      if (!drawing) { return }
-      drawing = false
-      drawLine(current.x, current.y, e.clientX || e.touches[0].clientX, e.clientY || e.touches[0].clientY, current.color, true)
-    }
-
-    // ----------- limit the number of events per second -----------------------
-
-    const throttle = (callback, delay) => {
-      let previousCall = new Date().getTime()
-      return function () {
-        const time = new Date().getTime()
-
-        if ((time - previousCall) >= delay) {
-          previousCall = time
-          callback.apply(null, arguments)
-        }
-      }
-    }
-
-    // -----------------add event listeners to our canvas ----------------------
-
-    canvas.addEventListener('mousedown', onMouseDown, false)
-    canvas.addEventListener('mouseup', onMouseUp, false)
-    canvas.addEventListener('mouseout', onMouseUp, false)
-    canvas.addEventListener('mousemove', throttle(onMouseMove, 10), false)
-
-    // Touch support for mobile devices
-    canvas.addEventListener('touchstart', onMouseDown, false)
-    canvas.addEventListener('touchend', onMouseUp, false)
-    canvas.addEventListener('touchcancel', onMouseUp, false)
-    canvas.addEventListener('touchmove', throttle(onMouseMove, 10), false)
-
-    // -------------- make the canvas fill its parent component -----------------
-
-    const onResize = () => {
-      canvas.width = window.innerWidth
-      canvas.height = window.innerHeight
-    }
-
-    window.addEventListener('resize', onResize, false)
-    onResize()
-
-    // ----------------------- socket.io connection ----------------------------
-    const onDrawingEvent = (data) => {
-      const w = canvas.width
-      const h = canvas.height
-      drawLine(data.x0 * w, data.y0 * h, data.x1 * w, data.y1 * h, data.color)
-    }
-
-    socketRef.current.on('drawing', onDrawingEvent)
-
+    socketRef.current.on('drawing', whiteboardRef.current.onDrawingEvent)
+    
     //handle new messages 
     const handleNewMessage = (message) => {
       setMessages(messages => [ ...messages, message ])
     }
     socketRef.current.on('message', handleNewMessage)
-
     return () => socketRef.current.disconnect()
-
+    
   }, [SERVER_ENDPOINT, user, roomId])
+
+
+
+  
+  const emitDrawing = (data) => {
+    socketRef.current.emit('drawing', data)
+  }
 
   const showRoomIdPrompt = () => {
     window.prompt('Copy to clipboard: Ctrl+C, Enter', roomId)
@@ -188,31 +68,10 @@ const RoomPage = ({ match }) => {
           </div>
         </div>
 
-        <div className='toolbox'>
-
-          <details className='tool tool__pen'>
-            <summary>Pen</summary>
-            <ul className='colors' ref={colorsRef}>
-              <div className='color black' />
-              <div className='color red' />
-              <div className='color green' />
-              <div className='color blue' />
-              <div className='color yellow' />
-            </ul>
-          </details>
-
-          <div 
-            className='tool tool__eraser' 
-            ref={eraserRef} 
-            id='eraser'
-          >
-            Eraser
-          </div>
-        </div>
+        <Whiteboard ref={whiteboardRef} emitDrawing={emitDrawing} />
 
         <RightPanel messages={messages} sendMessage={sendMessage} />
 
-        <canvas className='whiteboard' ref={canvasRef} />
 
       </div>
     </>
---
+++ src/Room/Whiteboard.jsx
@@ -0,0 +1,168 @@
+import React, { useEffect, useRef, useImperativeHandle, forwardRef } from 'react'
+
+const Whiteboard = forwardRef((props, ref) => {
+
+  const canvasRef = useRef(null)
+  const contextRef = useRef(null)
+  const colorsRef = useRef(null)
+  const eraserRef = useRef(null)
+  
+  useEffect(() => {
+    // --------------- getContext() method returns a drawing context on the canvas-----
+    const canvas = canvasRef.current
+    contextRef.current = canvas.getContext('2d')
+
+    // ----------------------- Colors --------------------------------------------------
+    const eraser = document.getElementById('eraser')
+    const colors = document.getElementsByClassName('color')
+
+    // set the current color
+    const current = {
+      color: 'black'
+    }
+
+    const getEraser = () => {
+      current.color = 'eraser'
+    }
+
+    eraser.addEventListener('click', getEraser, false)
+
+    // helper that will update the current color
+    const onColorUpdate = (e) => {
+      current.color = e.target.className.split(' ')[1]
+    }
+
+    // loop through the color elements and add the click event listeners
+    for (let i = 0; i < colors.length; i++) {
+      colors[i].addEventListener('click', onColorUpdate, false)
+    }
+    let drawing = false
+
+    // ---------------- mouse movement --------------------------------------
+
+    const onMouseDown = (e) => {
+      drawing = true
+      current.x = e.clientX || e.touches[0].clientX
+      current.y = e.clientY || e.touches[0].clientY
+    }
+
+    const onMouseMove = (e) => {
+      if (!drawing) { return }
+      drawLine(current.x, current.y, e.clientX || e.touches[0].clientX, e.clientY || e.touches[0].clientY, current.color, true)
+      current.x = e.clientX || e.touches[0].clientX
+      current.y = e.clientY || e.touches[0].clientY
+    }
+
+    const onMouseUp = (e) => {
+      if (!drawing) { return }
+      drawing = false
+      drawLine(current.x, current.y, e.clientX || e.touches[0].clientX, e.clientY || e.touches[0].clientY, current.color, true)
+    }
+
+    // ----------- limit the number of events per second -----------------------
+
+    const throttle = (callback, delay) => {
+      let previousCall = new Date().getTime()
+      return function () {
+        const time = new Date().getTime()
+
+        if ((time - previousCall) >= delay) {
+          previousCall = time
+          callback.apply(null, arguments)
+        }
+      }
+    }
+
+    // -----------------add event listeners to our canvas ----------------------
+
+    canvas.addEventListener('mousedown', onMouseDown, false)
+    canvas.addEventListener('mouseup', onMouseUp, false)
+    canvas.addEventListener('mouseout', onMouseUp, false)
+    canvas.addEventListener('mousemove', throttle(onMouseMove, 10), false)
+
+    // Touch support for mobile devices
+    canvas.addEventListener('touchstart', onMouseDown, false)
+    canvas.addEventListener('touchend', onMouseUp, false)
+    canvas.addEventListener('touchcancel', onMouseUp, false)
+    canvas.addEventListener('touchmove', throttle(onMouseMove, 10), false)
+
+    // -------------- make the canvas fill its parent component -----------------
+
+    const onResize = () => {
+      canvas.width = window.innerWidth
+      canvas.height = window.innerHeight
+    }
+
+    window.addEventListener('resize', onResize, false)
+    onResize()
+
+    
+  }, [])
+  
+  const drawLine = (x0, y0, x1, y1, color, emit) => {
+    if (color === 'eraser') {
+      contextRef.current.clearRect(x0, y0, 10, 10)
+    } else {
+      contextRef.current.beginPath()
+      contextRef.current.moveTo(x0, y0)
+      contextRef.current.lineTo(x1, y1)
+      contextRef.current.strokeStyle = color
+      contextRef.current.lineWidth = 2
+      contextRef.current.stroke()
+      contextRef.current.closePath()
+    }
+
+    if (!emit) { return }
+
+    const w = canvasRef.current.width
+    const h = canvasRef.current.height
+
+    props.emitDrawing({
+      x0: x0 / w,
+      y0: y0 / h,
+      x1: x1 / w,
+      y1: y1 / h,
+      color
+    })
+  }
+
+  useImperativeHandle(ref, () => ({
+    onDrawingEvent (data) {
+      const w = canvasRef.current.width
+      const h = canvasRef.current.height
+      drawLine(data.x0 * w, data.y0 * h, data.x1 * w, data.y1 * h, data.color)
+    }
+  }));
+  
+
+  return (
+    <>
+      <div className='toolbox'>
+
+        <details className='tool tool__pen'>
+          <summary>Pen</summary>
+          <ul className='colors' ref={colorsRef}>
+            <div className='color black' />
+            <div className='color red' />
+            <div className='color green' />
+            <div className='color blue' />
+            <div className='color yellow' />
+          </ul>
+        </details>
+
+        <div 
+          className='tool tool__eraser' 
+          ref={eraserRef} 
+          id='eraser'
+        >
+          Eraser
+        </div>
+
+      </div>
+
+      <canvas className='whiteboard' ref={canvasRef} />
+    </>
+  )
+})
+
+export default Whiteboard
